9ff3b9c7a1210abb5a04ad07a5d1f0f935fdc206
d296063 <d296063@edu.curio.nl>
2024-09-23 11:56:23 +0200

    made querry thing

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -10,8 +10,17 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 //1. Verbinding
 require_once '../../../config/conn.php';
 
-//2. Query
+$query = "INSERT INTO meldingen(attractie,type,capaciteit,prioriteit,melder,overige_info)
+VALUES(:attractie,:type,:capaciteit,:prioriteit,:melder,:overige_info)";
 
-//3. Prepare
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":attractie" => $attractie,
+        ":type" => $type,
+        ":capaciteit" => $capaciteit,
+        ":prioriteit" => $prioriteit,
+        ":melder" => $melder,
+        ":overige_info" => $overig,
+    ]);
 
-//4. Execute
+    header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
