78df1be776a6c3b788938019ad5c956024330a87
d296063 <d296063@edu.curio.nl>
2024-09-23 12:01:55 +0200

    made create.php

--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -15,14 +15,27 @@
         <h1>Nieuwe melding</h1>
 
         <form action="<?php echo $base_url; ?>/app/Http/Controllers/meldingenController.php" method="POST">
-
+            <input type="hidden" name="action" value="create">
             <div class="form-group">
                 <label for="attractie">Naam attractie:</label>
                 <input type="text" name="attractie" id="attractie" class="form-input">
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select type="text" name="type" id="type">
+                    <option value="">Kies je type</option>
+                    <option value="Achtbaan">Achtbaan</option>
+                    <option value="Draaiend">Draaiend</option>
+                    <option value="Kinder">Kinder</option>
+                    <option value="Horeca">Horeca</option>
+                    <option value="Show">Show</option>
+                    <option value="Water">Water</option>
+                    <option value="Overig">Overig</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="prioriteit">prioriteit:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
@@ -31,6 +44,10 @@
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+            <div class="form-group">
+                <label for="overige_info">Overige info:</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
