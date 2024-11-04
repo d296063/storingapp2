4c980b1c46c3eda0d3a40e64555ff63f3c5c60e1
d296063 <d296063@edu.curio.nl>
2024-11-04 13:14:10 +0100

    login.php gemaakt

---
+++ resources/views/login/login.php
@@ -0,0 +1,11 @@
+<form action="backend/loginController.php" method="post">
+    <div class="form-group">
+        <label for="username">Gebruikersnaam:</label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div class="form-group">
+        <label for="password">Wachtwoord:</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <input type="submit" value="login">
+</form>
